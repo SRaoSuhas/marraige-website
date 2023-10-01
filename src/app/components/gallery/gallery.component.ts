db24ccff01140266bd08c9d8cf04a848cf59b983
SRaoSuhas <suhassrao005@gmail.com>
2023-10-01 21:21:17 +0530

    image size reduced

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -13,6 +13,8 @@ export class GalleryComponent implements OnInit {
   previousScreenSize = 0;
   eventslist: string[] = [];
   columns: string[][] = [];
+  pagination: string[] = [];
+  activePage: number = 1
   ngOnInit() {
     this.data = JSONdata;
     this.previousScreenSize = innerWidth;
@@ -21,6 +23,7 @@ export class GalleryComponent implements OnInit {
     this.data.Gallery.Images.Events.forEach((item: any) => {
       this.eventslist.push(item.Event);
     });
+    this.createPagination(1);
   }
 
   ngAfterViewInit() {
@@ -33,6 +36,8 @@ export class GalleryComponent implements OnInit {
       this.previousScreenSize = innerWidth;
       this.screenSize();
     }
+    $('.paginationTiles').removeClass("active");
+    $('#paginationTiles-' + this.activePage).addClass("active");
   }
 
   screenSize() {
@@ -66,11 +71,13 @@ export class GalleryComponent implements OnInit {
       for (let i = 0; i < columnsNo; i++) {
         this.columns.push([]);
       }
-      for (let i = 0; i < this.data.Gallery.Images.Events[this.activeEventIndex].ImageIds.length; i++) {
-        const column = i % columnsNo;
-        this.columns[column].push(
-          this.data.Gallery.Images.Events[this.activeEventIndex].ImageIds[i]
-        );
+      for (let i = (this.activePage - 1) * 20; i < ((this.activePage - 1) * 20) + 20; i++) {
+        if (i < this.data.Gallery.Images.Events[this.activeEventIndex].ImageIds.length) {
+          const column = i % columnsNo;
+          this.columns[column].push(
+            this.data.Gallery.Images.Events[this.activeEventIndex].ImageIds[i]
+          );
+        }
       }
       this.activeImageId = this.columns[0][0];
     }
@@ -82,5 +89,56 @@ export class GalleryComponent implements OnInit {
 
   copyImgLink() {
     navigator.clipboard.writeText('https://drive.google.com/uc?export=view&id=' + this.activeImageId);
+  }
+
+  createPagination(activePage: number) {
+    this.pagination = [];
+    var totalPages = Math.ceil(this.data.Gallery.Images.Events[this.activeEventIndex].ImageIds.length / 20);
+    this.pagination.push("<<");
+    this.pagination.push("<");
+    if (totalPages <= 5 || activePage <= 3) {
+      for (var i = 1; i <= totalPages; i++) {
+        this.pagination.push(i.toString());
+      }
+    }
+    else if (activePage >= totalPages - 2) {
+      this.pagination.push((totalPages - 4).toString());
+      this.pagination.push((totalPages - 3).toString());
+      this.pagination.push((totalPages - 2).toString());
+      this.pagination.push((totalPages - 1).toString());
+      this.pagination.push(totalPages.toString());
+    }
+    else {
+      this.pagination.push((activePage - 2).toString());
+      this.pagination.push((activePage - 1).toString());
+      this.pagination.push(activePage.toString());
+      this.pagination.push((activePage + 1).toString());
+      this.pagination.push((activePage + 2).toString());
+    }
+    this.pagination.push(">");
+    this.pagination.push(">>");
+    this.activePage = activePage;
+  }
+
+  onPaginationClick(page: string) {
+    if (page != "<<" && page != "<" && page != ">" && page != ">>" && this.activePage != Number(page)) {
+      this.createPagination(Number(page));
+    }
+    else if (page == "<<") {
+      this.createPagination(1);
+    }
+    else if (page == "<" && this.activePage - 1 > 0) {
+      this.createPagination(this.activePage - 1);
+    }
+    else {
+      var totalPages = Math.ceil(this.data.Gallery.Images.Events[this.activeEventIndex].ImageIds.length / 20);
+      if (page == ">" && this.activePage + 1 <= totalPages) {
+        this.createPagination(totalPages + 1);
+      }
+      else if (page == ">>") {
+        this.createPagination(totalPages);
+      }
+    }
+    this.screenSize();
   }
 }
